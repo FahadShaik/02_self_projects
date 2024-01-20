0d8eacdcef8cfa79873b97f9e65202675c41b337
FahadShaik <shaikfahad904@gmail.com>
2024-01-20 21:18:33 +0530

    Successfully added the logic to add and subtract the budget from the app

--- 03_Expense_tracker/script.js
+++ 03_Expense_tracker/script.js
@@ -1,16 +1,33 @@
 const submitBtn = document.querySelector(".submit");
+const budgetForm = document.querySelector(".budget-form");
+const expenses = document.querySelector(".budget-category");
+const spentAmount = document.querySelector(".budget-amount");
 const budgetInput = document.querySelector(".budget-input");
-const budgetDisplayText = document.querySelector(".budget-display");
-const budgetDisplayContainer = document.querySelector(
+let budgetDisplayText = document.querySelector(".budget-display");
+let budgetDisplayContainer = document.querySelector(
   ".budget-display-container"
 );
-const budgetForm = document.querySelector(".budget-form");
+let budgetVal;
+
+let tableBody = document.querySelector("tbody");
 
 budgetForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  budgetDisplayText.innerHTML = budgetInput.value + " RS";
+  budgetVal = budgetInput.value;
+  budgetDisplayText.innerHTML = budgetVal + " Rs";
   budgetDisplayContainer.appendChild(budgetDisplayText);
-
   budgetInput.value = "";
 });
+
+function addCategory(budgetAmount) {
+  const expensesVal = expenses.value;
+  const spentAmountVal = spentAmount.value;
+
+  const template = `<tr>
+                        <td>${expensesVal}</td>
+                        <td>${spentAmountVal}</td>
+                        <td>${budgetAmount - spentAmountVal}</td>
+                    </tr>`;
+  tableBody.innerHTML = tableBody.innerHTML + template;
+}

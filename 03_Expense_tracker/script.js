6c7ad15d1810098c93b7f232f86929baf0fb7643
FahadShaik <shaikfahad904@gmail.com>
2024-01-22 15:58:02 +0530

    to empty field after updating the add

--- 03_Expense_tracker/script.js
+++ 03_Expense_tracker/script.js
@@ -1,7 +1,5 @@
 const submitBtn = document.querySelector(".submit");
 const budgetForm = document.querySelector(".budget-form");
-const expenses = document.querySelector(".budget-category");
-const spentAmount = document.querySelector(".budget-amount");
 const budgetInput = document.querySelector(".budget-input");
 let budgetDisplayText = document.querySelector(".budget-display");
 let budgetDisplayContainer = document.querySelector(
@@ -11,6 +9,8 @@ let budgetVal;
 
 let tableBody = document.querySelector("tbody");
 const error = document.querySelector(".err");
+let expenses = document.querySelector(".budget-category");
+let spentAmount = document.querySelector(".budget-amount");
 
 budgetForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -30,13 +30,16 @@ budgetForm.addEventListener("submit", (e) => {
 });
 
 function addCategory(budgetAmount) {
-  const expensesVal = expenses.value;
-  const spentAmountVal = spentAmount.value;
+  let expensesVal = expenses.value;
+  let spentAmountVal = spentAmount.value;
 
   const template = `<tr>
                         <td>${expensesVal}</td>
                         <td>${spentAmountVal}</td>
                         <td>${budgetAmount - spentAmountVal}</td>
-                    </tr>`;
+                      </tr>`;
   tableBody.innerHTML = tableBody.innerHTML + template;
+
+  expenses.value = "";
+  spentAmount.value = "";
 }

a0d35b61e4190ae97ee91e0cb845af35b0b10b29
FahadShaik <shaikfahad904@gmail.com>
2024-01-23 20:41:31 +0530

    Calci working with eval method

--- 04_Calculator/script.js
+++ 04_Calculator/script.js
@@ -1,8 +1,18 @@
 let numDisplay = document.querySelector(".num-display");
-const inputButton = document.querySelectorAll("button");
+const inputButton = document.querySelectorAll(".number-button");
+const clearInput = document.querySelector(".clear-button");
 
 inputButton.forEach((button) => {
   button.addEventListener("click", () => {
     numDisplay.value += button.textContent;
   });
+});
+
+function calculate() {
+  const finalResult = eval(numDisplay.value);
+  numDisplay.value = finalResult;
+}
+
+clearInput.addEventListener("click", () => {
+  numDisplay.value = "";
 });

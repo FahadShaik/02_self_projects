57c1b1384688b6a81878aeb460f684ba8c9350b0
FahadShaik <shaikfahad904@gmail.com>
2024-02-08 12:56:27 +0530

    Bootstrap and css styling for the same but a little clearer code

--- 06_Digital_clock/script.js
+++ 06_Digital_clock/script.js
@@ -1,9 +1,20 @@
-const digitalClock = document.getElementById("display-digiclock");
+const hourNum = document.getElementById("hr");
+const minNum = document.getElementById("min");
+const secNum = document.getElementById("sec");
+const amPm = document.getElementById("am-pm");
 
 const clockHandler = () => {
-  let clock = new Date().toLocaleTimeString();
+  let clock = new Date();
+  let twelveHrFormat = clock.toLocaleTimeString();
+  let hour = clock.getHours();
+  let min = clock.getMinutes();
+  let sec = clock.getSeconds();
+  let am_pm = hour >= 12 ? "pm" : "am";
 
-  digitalClock.innerHTML = clock;
+  hourNum.innerHTML = hour + " :";
+  minNum.innerHTML = min + " :";
+  secNum.innerHTML = sec;
+  amPm.innerHTML = am_pm;
 };
 
 setInterval(clockHandler, 1000);
